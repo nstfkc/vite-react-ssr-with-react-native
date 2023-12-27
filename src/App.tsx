b533100956e428eb916bac613361753281f6bb47
Enes Tufekci <enesxtufekci@gmail.com>
2023-12-27 11:44:47 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Test } from './Test'
+
+function App() {
+  return (
+    <>
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ src/Test.tsx
@@ -0,0 +1,7 @@
+import { View, Text} from 'react-native'
+
+export const Test = () => {
+    return <View>
+        <Text>React native web</Text>
+    </View>
+}
